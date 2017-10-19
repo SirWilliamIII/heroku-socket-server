6cb92f089ce5ea3a6ac9e82a3d2e625e92bc9c5c
SirWilliamIII <dev.wtc3@gmail.com>
2017-10-19 14:33:39 -0600

    added sockets on 4000

---
+++ index.js
@@ -0,0 +1,32 @@
+const server = require('net').createServer()
+let counter = 0
+let sockets = {}
+
+const port = 4000
+
+server.on('connection', socket => {
+
+	socket.id = counter++
+	sockets[socket.id] = socket
+
+	//socket.setEncoding('utf8')
+
+	console.log('Client connected');
+	socket.write('Welcome new client!\n')
+
+	socket.on('data', data => {
+		Object.entries(sockets).forEach(el => {
+			el[1].write(`${socket.id}: `)
+			el[1].write(data)
+		})
+	})
+
+	socket.on('end', () => {
+		delete sockets[socket.id]
+		console.log('Client Disconnected');
+	})
+})
+
+server.listen(port, () => {
+	console.log(`server listening on port: ${port}`)
+})
